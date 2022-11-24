7a2d70658c157a7809181918dd3f5c853749c28b
annisarahmah13 <49335273+annisarahmah13@users.noreply.github.com>
2022-11-24 15:29:29 +0700

    Update Data.sql

--- Data.sql
+++ Data.sql
@@ -1,4 +1,17 @@
-select year(order_date) as years,count(order_id) as total_orders,
-sum(sales) as total_sales from sales_report 
-where order_status='Order Finished'
-group by years order by years;
+SELECT year(order_date) AS years,
+COUNT(order_id) AS total_orders,
+SUM(sales) AS total_sales 
+FROM sales_report 
+WHERE order_status='Order Finished'
+GROUP BY years 
+ORDER BY years;
+
+Result :
+
+|years|total_orders|total_sales  |
+|-----|------------|-------------|
+|2,009|1,244       |4,613,872,681|
+|2,010|1,248       |4,059,100,607|
+|2,011|1,178       |4,112,036,186|
+|2,012|1,254       |4,482,983,158|
+
